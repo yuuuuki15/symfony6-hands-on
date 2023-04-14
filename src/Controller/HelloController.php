300ff1b1c3f11218f0dbdd72e0b90d2f7105e960
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-14 18:37:13 +0200

    create: twig file
    require twig and generate twig
    make response with rendering template

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = [
         "Hello", "Hi", "Bye!"
@@ -14,14 +15,22 @@ class HelloController
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): Response
     {
-        return new Response(
-            implode(',', array_slice($this->messages, 0, $limit))
-        );
+        return $this->render('hello/index.html.twig',
+            [
+                'message' => implode(',', array_slice($this->messages, 0, $limit))
+            ]
+    );
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
     public function shoeOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
+        // return new Response($this->messages[$id]);
     }
 }
