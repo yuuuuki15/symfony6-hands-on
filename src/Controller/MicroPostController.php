28b909489e8a474000426d2383df1b23394e5e55
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-16 17:42:53 +0200

    Symfony Forms
    composer require symfony/form

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
@@ -26,4 +27,22 @@ class MicroPostController extends AbstractController
             'post' => $post,
         ]);
     }
-}
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(): Response
+    {
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, ['label' => 'Save'])
+            ->getForm();
+
+        return $this->renderForm
+        ('micro_post/add.html.twig',
+            [
+                'form' =>$form
+            ]
+        );
+    }
+} 
