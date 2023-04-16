ef70301cbad90c797c431703d1a5eae5e56c3889
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-16 17:51:07 +0200

    Handling Form Submitssion

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -12,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
-    #[Route('/micro/post', name: 'app_micro_post')]
+    #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig', [
@@ -29,7 +30,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
         $microPost = new MicroPost();
         $form = $this->createFormBuilder($microPost)
@@ -37,6 +38,18 @@ class MicroPostController extends AbstractController
             ->add('text')
             ->add('submit', SubmitType::class, ['label' => 'Save'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post, true);
+
+            // Add a flash message
+            
+            // Redirect
+        }
 
         return $this->renderForm
         ('micro_post/add.html.twig',
