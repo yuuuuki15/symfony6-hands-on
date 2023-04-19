a5fd84a869218cd282d8ecc54750691a0c2c6b67
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-19 16:22:23 +0200

    relate post and comments to user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,24 +19,6 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $microPost1 = new MicroPost();
-        $microPost1->setTitle('Welcome to Poland!');
-        $microPost1->setText('Welcome to Poland1');
-        $microPost1->setCreated(new DateTime());
-        $manager->persist($microPost1);
-
-        $microPost2 = new MicroPost();
-        $microPost2->setTitle('Welcome to US!');
-        $microPost2->setText('Welcome to US1');
-        $microPost2->setCreated(new DateTime());
-        $manager->persist($microPost2);
-
-        $microPost3 = new MicroPost();
-        $microPost3->setTitle('Welcome to Germany!');
-        $microPost3->setText('Welcome to Germany1');
-        $microPost3->setCreated(new DateTime());
-        $manager->persist($microPost3);
-
         $user1 = new User();
         $user1->setEmail('test@test.com');
         $user1->setPassword(
@@ -56,6 +38,27 @@ class AppFixtures extends Fixture
             )
         );
         $manager->persist($user2);
+
+        $microPost1 = new MicroPost();
+        $microPost1->setTitle('Welcome to Poland!');
+        $microPost1->setText('Welcome to Poland1');
+        $microPost1->setCreated(new DateTime());
+        $microPost1->setAuthor($user1);
+        $manager->persist($microPost1);
+
+        $microPost2 = new MicroPost();
+        $microPost2->setTitle('Welcome to US!');
+        $microPost2->setText('Welcome to US1');
+        $microPost2->setCreated(new DateTime());
+        $microPost2->setAuthor($user2);
+        $manager->persist($microPost2);
+
+        $microPost3 = new MicroPost();
+        $microPost3->setTitle('Welcome to Germany!');
+        $microPost3->setText('Welcome to Germany1');
+        $microPost3->setCreated(new DateTime());
+        $microPost3->setAuthor($user1);
+        $manager->persist($microPost3);
 
         $manager->flush();
     }
