f304c4cd9e7d21bd82d81c48aa26efe7a46a2fd8
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-19 16:12:13 +0200

    logging user out

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -22,4 +22,9 @@ class LoginController extends AbstractController
             'error' => $error
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+    }
 }
