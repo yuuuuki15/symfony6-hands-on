208a30ed5697e907b292e4e9fdc869a15ab15d8f
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-21 17:18:00 +0200

    embedding forms

--- src/Controller/SettingsProfileController.php
+++ src/Controller/SettingsProfileController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserProfile;
 use App\Form\UserProfileType;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SettingsProfileController extends AbstractController
 {
     #[Route('/settings/profile', name: 'app_settings_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function profile(
-        Request $request
+        Request $request,
+        UserRepository $users
     ): Response
     {
         /** @var User $user */
@@ -28,9 +31,16 @@ class SettingsProfileController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()){
             $userProfile = $form->getData();
-            // save this somehow
-            // add the flash message
-            // redirect
+            $user->setUserProfile($userProfile);
+            $users->save($user, true);
+            $this->addFlash(
+                'success',
+                'Your user profile settings were saved.'
+            );
+
+            return $this->redirectToRoute(
+                'app_settings_profile'
+            );
         }
         return $this->render('settings_profile/profile.html.twig', [
             'form' => $form->createView(),
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Form\UserProfileType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -54,6 +55,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('userProfile', UserProfileType::class)
         ;
     }
 
