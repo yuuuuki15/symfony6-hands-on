77c86f110c094884176ec89585f924bc1e8f46c5
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-21 16:58:03 +0200

    user profile form

---
+++ src/Controller/SettingsProfileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Form\UserProfileType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SettingsProfileController extends AbstractController
+{
+    #[Route('/settings/profile', name: 'app_settings_profile')]
+    public function profile(
+        Request $request
+    ): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $userProfile = $user->getUserProfile() ?? new UserProfile();
+
+        $form = $this->createForm(
+            UserProfileType::class, $userProfile
+        );
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $userProfile = $form->getData();
+            // save this somehow
+            // add the flash message
+            // redirect
+        }
+        return $this->render('settings_profile/profile.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
