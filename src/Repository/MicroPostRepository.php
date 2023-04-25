a0ff3fda3f8c6ceda7167df18ec07eb72323b460
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2023-04-25 12:26:15 +0200

    user posts - optimize query

--- src/Repository/MicroPostRepository.php
+++ src/Repository/MicroPostRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\User;
 use App\Entity\MicroPost;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
@@ -47,6 +48,25 @@ class MicroPostRepository extends ServiceEntityRepository
         )->getQuery()->getResult();
     }
 
+    public function findAllByAuthor(
+        int | User $author
+    ): array
+    {
+        return $this->findAllQuery(
+            withComments: true,
+            withLikes: true,
+            withAuthors: true,
+            withProfiles: true
+        )
+        ->where('p.author = :author')
+        ->setParameter(
+            'author',
+            $author instanceof User? $author->getId() : $author
+        )
+        ->getQuery()
+        ->getResult();
+    }
+
     private function findAllQuery(
         bool $withComments = false,
         bool $withLikes = false,
@@ -60,7 +80,7 @@ class MicroPostRepository extends ServiceEntityRepository
             ->addSelect('c');
         }
         if($withLikes){
-            $query->leftJoin('p.likedNy', 'l')
+            $query->leftJoin('p.likedBy', 'l')
             ->addSelect('l');
         }
         if($withAuthors || $withProfiles){
